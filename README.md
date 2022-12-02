59402f043567c63fca142b288c28b1c49b893a67
Sean Hignett <sean@idstudios.io>
2022-12-02 10:39:16 -0700

    initial clean commit

---
+++ README.md
@@ -0,0 +1,82 @@
+Cluster Builder for VirtualBox
+==============================
+
+Multi-node Kubernetes clusters in VirtualBox.
+
+>Still in the early stages...
+
+>There must be over a half dozen different ways to get K8s on your desktop these days, but none of them are a very good likeness to real kubernetes.  Nearly all are single node master workloads - and that isn't really a cluster.  I found myself missing the true K8s experience I used to have with [cluster builder](https://github.com/ids/cluster-builder).
+
+>VMware jumped the shark.  No point in looking back.  
+
+>It was time to do a VirtualBox edition.
+
+## Requirements:
+
+- VirtualBox
+- Packer
+- Ansible
+
+This has been tested on a __macOS__ host thus far and likely won't migrate much further.
+
+## Cluster Config Files
+In the style of __cluster-builder__ the ansible inventory host files are the K8s configuration files, and are stored in:
+
+```
+ clusters/[org folder]/[cluster name folder]/hosts
+```
+
+For example:
+
+```
+#clusters/local/k8s/hosts
+
+[all:vars]
+cluster_name=k8s-local
+remote_user=sysop
+
+network_mask=255.255.255.0
+network_cidr=/24
+network_dn=vm.idstudios.io
+
+[k8s_masters]
+k8s-m1.vm.idstudios.io ansible_host=192.168.56.50 numvcpus=2 memsize=2524
+
+[k8s_workers]
+k8s-w1.vm.idstudios.io ansible_host=192.168.56.60 numvcpus=2 memsize=2524
+k8s-w2.vm.idstudios.io ansible_host=192.168.56.61 numvcpus=2 memsize=2524
+
+```
+
+There is an example file in __clusters/eg__
+
+## Create a Kubernetes Cluster
+
+```
+bash build-cluster eg/k8s
+```
+
+This has 2-3 phases:
+
+1. Packer build of cluster node image (only happens once)
+2. Ansible deployment of cluster node image to VirtualBox nodes defined in hosts file
+3. Ansible deployment of Kubernetes to nodes
+
+After the cluster node is built, stage one is bypassed.  To rebuild the cluster-node (which takes some time), you can remove the image:
+
+```
+rm -rf node-packer/images
+```
+
+And then stage one will repeat and a fresh __cluster-node.ova__ will be built by packer.
+
+When the cluster has finished a message will be displayed with instructions for using the cluster.  The `kubeconfig` file is downloaded to the __cluster package folder__ (eg. eg/k8s), which you can then merge to your ~/.kube/config, or reference explicitly.
+
+
+## Control Kubernetes Cluster VMs
+
+```
+bash control-cluster eg/k8s [start | stop | pause | resume | savestate]
+```
+
+This makes it easy to suspend a functioning cluster and resume it later, which is very useful in development.
---
+++ node-packer/bin/ubuntu-bootstrap.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo '>>> Installing base dependencies'
+sudo apt -y install libssl-dev apt-transport-https ca-certificates \
+git wget chrony curl bash tar nano gnupg2 software-properties-common
+
+echo '>>> Appending Authorized Keys for [sysop]'
+mkdir -p /home/sysop/.ssh
+chown -R sysop:sysop /home/sysop/.ssh
+chmod 700 /home/sysop/.ssh
+cp /tmp/authorized_keys /home/sysop/.ssh/
+chown -R sysop:sysop /home/sysop/.ssh/authorized_keys
+chmod 600 /home/sysop/.ssh/authorized_keys
+
+echo '>>> Updating Ubuntu'
+sudo apt update 
+sudo apt-get upgrade -y
+
+echo '>>> Disable Swap'
+sudo swapoff --all
+
+echo '>>> Enable Kernel Modules'
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+echo '>>> Configure Sysctl'
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+echo '>>> Reload Sysctl'
+sudo sysctl --system
+
+echo '>>> Install K8s Repo'
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository --yes "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo add-apt-repository --yes multiverse
+sudo apt update
+
+echo '>>> Install K8s Binaries'
+sudo apt-get install -y kubelet kubeadm kubectl
+
+echo '>>> Install Containerd'
+wget https://github.com/containerd/containerd/releases/download/v1.6.10/containerd-1.6.10-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-1.6.10-linux-amd64.tar.gz
+
+# Add Docker repo
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install containerd
+#sudo apt update
+#sudo apt install -y containerd.io
+
+sudo mkdir -p /etc/containerd/
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo curl -L https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /etc/systemd/system/containerd.service
+
+echo '>>> Install Runc'
+wget https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+#echo '>>> Install CNI and configure Containerd service'
+#wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+#sudo mkdir -p /opt/cni/bin
+#sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+sudo systemctl daemon-reload
+
+echo '>>> Start and Enable Containerd service'
+sudo systemctl start containerd 
+sudo systemctl enable containerd
+
+echo '>>> Pre-seed node image with containers'
+sudo kubeadm config images pull
+
+#echo '>>> Install VBox Guest Additions'
+sudo apt install -y virtualbox-guest-utils virtualbox-guest-x11
+
+sudo cat <<EOF > ~/00-installer-config.yaml
+network:
+  ethernets:
+    enp0s3:
+      dhcp4: true
+      dhcp-identifier: mac
+    enp0s8:
+      dhcp4: true
+      dhcp-identifier: mac
+  version: 2
+EOF
+
+sudo rm /etc/netplan/00-installer-config.yaml
+sudo cp ~/00-installer-config.yaml /etc/netplan/00-installer-config.yaml
+
+echo '>>> Netplan'
+sudo cat /etc/netplan/00-installer-config.yaml
+sudo netplan apply
+
+echo '>>> /etc/hosts'
+sudo cat /etc/hosts
+
